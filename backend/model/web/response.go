b1abb763f876e548c492c49ef30847965a6a35e9
Dana Wandana <dn.wandana@gmail.com>
2024-03-22 16:31:57 +0700

    chore: split response struct

--- backend/model/web/response.go
+++ backend/model/web/response.go
@@ -1,7 +1,13 @@
 package web
 
-type Response struct {
+// ResponseData is a struct that represents the response data format
+type ResponseData struct {
+	Status bool `json:"status"`
+	Data   any  `json:"data"`
+}
+
+// ResponseMessage is a struct that represents the response message format
+type ResponseMessage struct {
 	Status  bool   `json:"status"`
 	Message string `json:"message"`
-	Data    any    `json:"data"`
 }
