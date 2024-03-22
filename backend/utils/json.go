73712f1ca6be27491928977d37cb2e8e99f79cc5
Dana Wandana <dn.wandana@gmail.com>
2024-03-22 16:34:55 +0700

    chore: adjust WriteJsonResponse function parameter

--- backend/utils/json.go
+++ backend/utils/json.go
@@ -3,8 +3,6 @@ package utils
 import (
 	"encoding/json"
 	"net/http"
-
-	"github.com/dnwandana/expense-tracker/model/web"
 )
 
 // ReadJsonRequest is a function that decodes the JSON body of the HTTP request into the provided empty interface as the result.
@@ -15,7 +13,7 @@ func ReadJsonrequest(request *http.Request, result any) {
 
 // WriteJsonResponse is a function that sets the content type of the HTTP response to "application/json".
 // Then, it encodes the web.Response object into a JSON format and writes it into the HTTP response.
-func WriteJsonResponse(writer http.ResponseWriter, response web.Response) {
+func WriteJsonResponse(writer http.ResponseWriter, response any) {
 	writer.Header().Set("Content-Type", "application/json")
 	err := json.NewEncoder(writer).Encode(response)
 	PanicIfError(err)
